69e007005232d9fe9dccd2404ee4945f57534784
Daniel Nelson <dnelson@centresource.com>
2011-09-26 09:13:12 -0500

    pull view code out of controller into widget

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,6 @@
 //= require jquery
-//= require initialize
 //= require jquery.cycle
 //= require services
-//= require helpers
 //= require controllers
 //= require filters
 //= require widgets 
--- app/assets/javascripts/widgets.js
+++ app/assets/javascripts/widgets.js
@@ -1,1 +1,26 @@
 /* http://docs.angularjs.org/#!angular.widget */
+
+angular.directive("my:cycle", function(expr,el){
+   return function(container){
+        var scope = this;
+        var lastChildID = container.children().last().attr('id');
+
+        var doIt = function() {
+            var lastID = container.children().last().attr('id');
+            if (lastID != lastChildID) {
+                lastChildID = lastID;
+                $(container).cycle({ fx: 'fade',
+                                     speed: 500,
+                                     timeout: 3000,
+                                     pause: 1,
+                                     next: '#next',
+                                     prev: '#prev'});
+            }
+        }
+
+        var defer = this.$service("$defer");
+        scope.$parent.$onEval( function() {
+            defer(doIt);
+        });
+   }
+});
