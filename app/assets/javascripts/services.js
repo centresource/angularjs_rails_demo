eb6ddc36d7bf076e5b5b329f35b39e43004dcd03
Igor Minar <iiminar@gmail.com>
2011-11-15 12:30:54 -0800

    make resources more slide friendly

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -1,20 +1,24 @@
 /* http://docs.angularjs.org/#!angular.service */
 
 angular.service('Photographers', function($resource) {
- return $resource('photographers/:photographer_id', {}, { 'index': { method: 'GET', isArray: true }});
+ return $resource('photographers/:photographer_id', {},
+                  { 'index': { method: 'GET', isArray: true }});
 });
 
 angular.service('Galleries', function($resource) {
- return $resource('photographers/:photographer_id/galleries/:gallery_id', {}, { 'index': { method: 'GET', isArray: true }});
+ return $resource('photographers/:photographer_id/galleries/:gallery_id', {},
+                  { 'index': { method: 'GET', isArray: true }});
 });
 
 angular.service('Photos', function($resource) {
- return $resource('photographers/:photographer_id/galleries/:gallery_id/photos', {}, { 'index': { method: 'GET', isArray: true }});
+ return $resource('photographers/:photographer_id/galleries/:gallery_id/photos', {},
+                  { 'index': { method: 'GET', isArray: true }});
 });
 
 angular.service('SelectedPhotos', function($resource) {
- return $resource('selected_photos/:selected_photo_id', {}, { 'create': { method: 'POST' },
-                                                              'index': { method: 'GET', isArray: true },
-                                                              'update': { method: 'PUT' },
-                                                              'destroy': { method: 'DELETE' }});
+ return $resource('selected_photos/:selected_photo_id', {},
+                    { 'create': { method: 'POST' },
+                      'index': { method: 'GET', isArray: true },
+                      'update': { method: 'PUT' },
+                      'destroy': { method: 'DELETE' }});
 });
